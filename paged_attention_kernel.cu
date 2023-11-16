0c349a1053b8a1055b5f96c2a3bb4f964f6ab020
Hyunsung Lee <ita9naiwa@gmail.com>
2023-11-16 19:03:01 +0900

    Add  continuous batching

--- paged_attention_kernel.cu
+++ paged_attention_kernel.cu
@@ -46,8 +46,8 @@ __global__ void paged_attention_forward_kernel(
             if (i >= j)
             {
                 for(int k = 0; k < dim; ++k) {
-                    int Q_idx = (dim * num_heads) * shifted_i + k;
-                    int K_idx = (dim * num_heads) * shifted_j + k;
+                    int Q_idx = (dim * num_heads) * shifted_i + dim * head_id + k;
+                    int K_idx = (dim * num_heads) * shifted_j + dim * head_id + k;
                     S[S_idx] += Q[Q_idx] * K[K_idx] * scale;
                 }
             } else {
@@ -58,6 +58,7 @@ __global__ void paged_attention_forward_kernel(
     float val_sum;
     const int idx_beg = (num_heads * max_context_len * max_context_len) * block_id + \
                         (max_context_len * max_context_len) * head_id;
+    // batch_size, num_heads, max_context_len, max_context_len
     for (int i = 0; i < max_context_len; ++i) {
         val_sum = 1e-9;
         for(int j = thread_id; j < size; j += block_dim) {
@@ -79,9 +80,8 @@ __global__ void paged_attention_forward_kernel(
                 int P_idx = (num_heads * max_context_len * max_context_len) * block_id + \
                             (max_context_len * max_context_len) * head_id + \
                             (max_context_len) * i + j;
-                int V_idx = (num_heads * dim) * shifted_j + k;
-                int O_idx = (num_heads * dim) * shifted_i + k;
-                printf("%0.4f %0.4f\n", P[P_idx], V[V_idx]);
+                int V_idx = (num_heads * dim) * shifted_j + dim * head_id + k;
+                int O_idx = (num_heads * dim) * shifted_i + dim * head_id + k;
                 O[O_idx] += P[P_idx] * V[V_idx];
             }
         }
@@ -90,17 +90,17 @@ __global__ void paged_attention_forward_kernel(
 
 template <typename scalar_t>
 __global__ void paged_kv_attention_forward_kernel(
-    const int context_len,
+    const int max_context_len,
     const int dim,
     const float scale,
-    scalar_t* __restrict__ Q,           // [batch_size, num_heads, dim]
-    scalar_t* __restrict__ K,           // [batch_size, num_heads, dim]
-    scalar_t* __restrict__ V,           // [batch_size, num_heads, dim]
-    scalar_t* __restrict__ K_cache,     // [batch_size, context_len, num_heads, dim]
-    scalar_t* __restrict__ V_cache,     // [batch_size, context_len, num_heads, dim]
-    scalar_t* __restrict__ S,           // [batch_size, num_heads, context_len + 1]
-    scalar_t* __restrict__ P,           // [batch_size, num_heads, context_len + 1]
-    scalar_t* __restrict__ O            // [batch_size, num_heads, dim]
+    scalar_t* __restrict__ Q,               // [length, num_heads, dim]
+    scalar_t* __restrict__ K_cache,         // [batch_size, context_len, num_heads, dim]
+    scalar_t* __restrict__ V_cache,         // [batch_size, context_len, num_heads, dim]
+    int* __restrict__ cache_indices,        // [batch_size, context_len, num_heads, dim]
+    int* __restrict__ offsets,              // [batch_size, context_len, num_heads, dim]
+    scalar_t* __restrict__ S,               // [batch_size, num_heads, context_len + 1]
+    scalar_t* __restrict__ P,               // [batch_size, num_heads, context_len + 1]
+    scalar_t* __restrict__ O                // [batch_size, num_heads, dim]
 ) {
     const int thread_id = threadIdx.x;
     const int block_dim = blockDim.x;
@@ -158,28 +158,31 @@ std::vector<torch::Tensor> paged_attention_forward(
 }
 
 std::vector<torch::Tensor> paged_kv_attention_forward(
-    torch::Tensor &Q,       // [batch_size, dim]
-    torch::Tensor &K,       // [batch_size, dim]
-    torch::Tensor &V,       // [batch_size, dim]
-    torch::Tensor &K_cache, // [batch_size, context_len, dim]
-    torch::Tensor &V_cache, // [batch_size, context_len, dim]
+    torch::Tensor &Q,                   // [batch_size, dim]
+    torch::Tensor &K_cache,             // [num tokens, num_heads, dim]
+    torch::Tensor &V_cache,             // [num tokens, num_heads, dim]
+    torch::Tensor &cache_indices,       // [num total working indices]
+    torch::Tensor &offsets,              // [batch_size]
     int num_heads
 ) {
-    CHECK_INPUT(Q); CHECK_INPUT(K); CHECK_INPUT(V);
-    CHECK_INPUT(K_cache); CHECK_INPUT(V_cache);
+    CHECK_INPUT(Q); CHECK_INPUT(K_cache); CHECK_INPUT(V_cache);
 
     auto batch_size = Q.size(0);
     auto dim = Q.size(1);
-    auto context_len = K_cache.size(1);
     assert(dim % num_heads == 0);
 
     auto options = torch::TensorOptions().dtype(Q.scalar_type()).device(torch::kCUDA);
 
-    auto S = torch::zeros({batch_size, num_heads, context_len + 1}, options);
-    auto P = torch::zeros({batch_size, num_heads, context_len + 1}, options);
-    auto O = torch::zeros({batch_size, dim}, options);
 
-    const int threads = std::min((int) (context_len + 1), 1024);
+    int max_context_len = 0;
+    for (int i = 1; i < batch_size; ++i)
+    max_context_len = max(max_context_len, (offsets[i] - offsets[i - 1]).item<int>());
+
+    auto S = torch::zeros({batch_size, num_heads, max_context_len + 1}, options);
+    auto P = torch::zeros({batch_size, num_heads, max_context_len + 1}, options);
+    auto O = torch::zeros_like(Q);
+
+    const int threads = std::min((int) (max_context_len + 1), 1024);
     const dim3 blocks(batch_size, num_heads);
     const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
@@ -189,15 +192,15 @@ std::vector<torch::Tensor> paged_kv_attention_forward(
         Q.scalar_type(),
         "paged_kv_attention_forward_kernel",
         ([&] {
-            paged_kv_attention_forward_kernel<<<blocks, threads, context_len, stream>>>(
-                context_len,
+            paged_kv_attention_forward_kernel<<<blocks, threads, max_context_len, stream>>>(
+                max_context_len,
                 dim / num_heads,
                 scale,
                 Q.data_ptr<scalar_t>(),
-                K.data_ptr<scalar_t>(),
-                V.data_ptr<scalar_t>(),
                 K_cache.data_ptr<scalar_t>(),
                 V_cache.data_ptr<scalar_t>(),
+                cache_indices.data_ptr<int>(),
+                offsets.data_ptr<int>(),
                 S.data_ptr<scalar_t>(),
                 P.data_ptr<scalar_t>(),
                 O.data_ptr<scalar_t>()
