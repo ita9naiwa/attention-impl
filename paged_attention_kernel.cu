8e17f03e9e470c5b9a0e3d0804b33844b524d96d
Hyunsung Lee <ita9naiwa@gmail.com>
2023-11-17 12:30:50 +0900

    add pagedattention

--- paged_attention_kernel.cu
+++ paged_attention_kernel.cu
@@ -90,7 +90,7 @@ __global__ void paged_attention_forward_kernel(
 
 template <typename scalar_t>
 __global__ void paged_kv_attention_forward_kernel(
-    const int context_len,
+    const int max_context_len,
     const int dim,
     const float scale,
     scalar_t* __restrict__ Q,               // [length, num_heads, dim]
@@ -99,8 +99,9 @@ __global__ void paged_kv_attention_forward_kernel(
     scalar_t* __restrict__ K_cache,         // [cache_size, num_heads, dim]
     scalar_t* __restrict__ V_cache,         // [cache_size, num_heads, dim]
     int* __restrict__ cache_indices,        // [length]
-    scalar_t* __restrict__ S,               // [batch_size, num_heads, context_len + 1]
-    scalar_t* __restrict__ P,               // [batch_size, num_heads, context_len + 1]
+    int* __restrict__ offsets,              // [batch_size]
+    scalar_t* __restrict__ S,               // [batch_size, num_heads, max_context_len + 1]
+    scalar_t* __restrict__ P,               // [batch_size, num_heads, max_context_len + 1]
     scalar_t* __restrict__ O                // [length, num_heads, dim]
 ) {
     const int thread_id = threadIdx.x;
@@ -109,14 +110,18 @@ __global__ void paged_kv_attention_forward_kernel(
     const int head_id = blockIdx.y;
     const int batch_size = gridDim.x;
     const int num_heads = gridDim.y;
+
+    const int beg_idx = (block_id == 0)? 0 : offsets[block_id - 1];
+    const int end_idx = offsets[block_id];
+    const int size = end_idx - beg_idx;
+
      // S[i] = K_cache[i][j] * Q[j];
-    for(int i = thread_id; i < context_len; i += block_dim) {
-        int S_idx = ((1 + context_len) * num_heads) * block_id + \
-                    (1 + context_len) * head_id + i;
+    printf("block_id %d beg_id %d size %d\n",block_id, beg_idx, size);
+    for(int i = thread_id; i < size; i += block_dim) {
+        int S_idx = ((1 + max_context_len) * num_heads) * block_id + \
+                    (1 + max_context_len) * head_id + i;
         for(int j = 0;j < dim; ++j) {
-            int K_cache_idx = (context_len * num_heads * dim) * block_id + \
-                              (dim) * head_id + \
-                              (num_heads * dim) * cache_indices[i] + j;
+            int K_cache_idx = (dim) * head_id + (num_heads * dim) * cache_indices[beg_idx + i] + j;
             int Q_idx = (num_heads * dim) * block_id + dim * head_id + j;
             S[S_idx] += K_cache[K_cache_idx] * Q[Q_idx];
         }
@@ -129,39 +134,37 @@ __global__ void paged_kv_attention_forward_kernel(
         tmp += Q[Q_idx] * K[K_idx];
     }
 
-    // S shape [batch_size, num_heads, context_len + 1]
-    S[(num_heads * (1 + context_len)) * block_id + (context_len + 1) * head_id + context_len] = blockReduceSum<float>(tmp);
+    // S shape [batch_size, num_heads, max_context_len + 1]
+    S[(num_heads * (1 + max_context_len)) * block_id + (max_context_len + 1) * head_id + size] = blockReduceSum<float>(tmp);
 
 
     float exp_sum = 0;
-    for(int i = thread_id; i < context_len + 1; i += block_dim) {
-        int idx = ((1 + context_len) * num_heads) * block_id + (1 + context_len) * head_id + i;
+    for(int i = thread_id; i < size + 1; i += block_dim) {
+        int idx = ((1 + max_context_len) * num_heads) * block_id + (1 + max_context_len) * head_id + i;
         float exp_val = exp(S[idx]);
         exp_sum += exp_val;
     }
 
     exp_sum = blockReduceSum<float>(exp_sum);
-    for(int i = thread_id; i < context_len + 1; i += block_dim) {
-        int idx = ((1 + context_len) * num_heads) * block_id + (1 + context_len) * head_id + i;
+    for(int i = thread_id; i < size + 1; i += block_dim) {
+        int idx = ((1 + max_context_len) * num_heads) * block_id + (1 + max_context_len) * head_id + i;
         float exp_val = exp(S[idx]);
         P[idx] = exp_val / exp_sum;
     }
 
     for (int j = thread_id; j < dim; j += block_dim) {
-        for (int i = 0; i < context_len; ++i) {
+        for (int i = 0; i < size; ++i) {
             int O_idx = (num_heads * dim) * block_id + (dim) * head_id + j;
-            int P_idx = (num_heads * (1 + context_len)) * block_id + \
-                        ((1 + context_len)) * head_id + i;
-            int V_idx = (context_len * num_heads * dim) * block_id + \
-                        dim * head_id + \
-                        (num_heads * dim) * cache_indices[i] + j;
+            int P_idx = (num_heads * (1 + max_context_len)) * block_id + \
+                        ((1 + max_context_len)) * head_id + i;
+            int V_idx = dim * head_id + (num_heads * dim) * cache_indices[beg_idx + i] + j;
             O[O_idx] += P[P_idx] * V_cache[V_idx];
         }
     }
     for (int j = thread_id; j < dim; j += block_dim) {
         int O_idx = (num_heads * dim) * block_id + (dim) * head_id + j;
-        int P_idx = (num_heads * (1 + context_len)) * block_id + \
-                    ((1 + context_len)) * head_id + context_len;
+        int P_idx = (num_heads * (1 + max_context_len)) * block_id + \
+                    ((1 + max_context_len)) * head_id + size;
         int V_idx = (num_heads * dim) * block_id + (dim) * head_id + j;
         O[O_idx] += P[P_idx] * V[V_idx];
     }
@@ -181,9 +184,10 @@ std::vector<torch::Tensor> paged_attention_forward(
     auto dim = Q.size(1);
     assert(dim % num_heads == 0);
     auto options = torch::TensorOptions().dtype(Q.scalar_type()).device(torch::kCUDA);
-    int max_context_len = 0;
-    for (int i = 1; i < batch_size; ++i)
+    int max_context_len = offsets[0].item<int>();
+    for (int i = 1; i < batch_size; ++i) {
         max_context_len = max(max_context_len, (offsets[i] - offsets[i - 1]).item<int>());
+    }
 
     auto S = torch::zeros({batch_size, num_heads, max_context_len, max_context_len}, options);
     auto P = torch::zeros({batch_size, num_heads, max_context_len, max_context_len}, options);
@@ -222,6 +226,7 @@ std::vector<torch::Tensor> paged_kv_attention_forward(
     torch::Tensor &K_cache,             // [num tokens, num_heads, dim]
     torch::Tensor &V_cache,             // [num tokens, num_heads, dim]
     torch::Tensor &cache_indices,       // [num total working indices]
+    torch::Tensor &offsets,             // [batch_size]
     int num_heads
 ) {
     CHECK_INPUT(Q); CHECK_INPUT(K); CHECK_INPUT(V);
@@ -233,13 +238,16 @@ std::vector<torch::Tensor> paged_kv_attention_forward(
 
     auto options = torch::TensorOptions().dtype(Q.scalar_type()).device(torch::kCUDA);
 
-    auto context_len = cache_indices.size(0);
+    auto max_context_len = offsets[0].item<int>();
+    for(int i = 1; i < batch_size; ++i) {
+        max_context_len = max(max_context_len, (offsets[i] - offsets[i - 1]).item<int>());
+    }
 
-    auto S = torch::zeros({batch_size, num_heads, context_len + 1}, options);
-    auto P = torch::zeros({batch_size, num_heads, context_len + 1}, options);
+    auto S = torch::zeros({batch_size, num_heads, max_context_len + 1}, options);
+    auto P = torch::zeros({batch_size, num_heads, max_context_len + 1}, options);
     auto O = torch::zeros_like(Q);
 
-    const int threads = std::min((int) (context_len + 1), 1024);
+    const int threads = std::min((int) (max_context_len + 1), 1024);
     const dim3 blocks(batch_size, num_heads);
     const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
@@ -249,8 +257,8 @@ std::vector<torch::Tensor> paged_kv_attention_forward(
         Q.scalar_type(),
         "paged_kv_attention_forward_kernel",
         ([&] {
-            paged_kv_attention_forward_kernel<<<blocks, threads, context_len, stream>>>(
-                context_len,
+            paged_kv_attention_forward_kernel<<<blocks, threads, max_context_len, stream>>>(
+                max_context_len,
                 dim / num_heads,
                 scale,
                 Q.data_ptr<scalar_t>(),
@@ -259,6 +267,7 @@ std::vector<torch::Tensor> paged_kv_attention_forward(
                 K_cache.data_ptr<scalar_t>(),
                 V_cache.data_ptr<scalar_t>(),
                 cache_indices.data_ptr<int>(),
+                offsets.data_ptr<int>(),
                 S.data_ptr<scalar_t>(),
                 P.data_ptr<scalar_t>(),
                 O.data_ptr<scalar_t>()
--- test_kv_paged_attention.py
+++ test_kv_paged_attention.py
@@ -3,17 +3,23 @@ import numpy as np
 import time
 from paged_attention import paged_kv_attention_forward
 
+
+np.random.seed(42)
+torch.manual_seed(42)
+
 std = 0.1
-batch_size = 1
-dim = 16
-num_heads = 4
+batch_size = 2
+dim = 4
+num_heads = 1
 cache_size = 1024
-Q = torch.normal(mean=0, std=std, size=(1, dim)).cuda().to(torch.float32)
-K = torch.normal(mean=0, std=std, size=(1, dim)).cuda().to(torch.float32)
-V = torch.normal(mean=0, std=std, size=(1, dim)).cuda().to(torch.float32)
+Q = torch.normal(mean=0, std=std, size=(batch_size, dim)).cuda().to(torch.float32)
+K = torch.normal(mean=0, std=std, size=(batch_size, dim)).cuda().to(torch.float32)
+V = torch.normal(mean=0, std=std, size=(batch_size, dim)).cuda().to(torch.float32)
 K_cache = torch.normal(mean=0, std=std, size=(cache_size, dim)).cuda().to(torch.float32)
 V_cache = torch.normal(mean=0, std=std, size=(cache_size, dim)).cuda().to(torch.float32)
-cache_indices = torch.LongTensor([16, 24]).to(torch.int32).cuda()
+cache_indices = torch.LongTensor([16, 24, 1, 2]).to(torch.int32).cuda()
+offsets = torch.LongTensor([2, 4]).to(torch.int32).cuda()
+
 def reference_kv_MHA(Q, K, V, K_cache, V_cache, cache_indices):
     scale = (dim / num_heads) ** -0.5
     Q = Q.reshape(batch_size, num_heads, dim // num_heads)
@@ -21,8 +27,8 @@ def reference_kv_MHA(Q, K, V, K_cache, V_cache, cache_indices):
     V = V.reshape(batch_size, num_heads, dim // num_heads)
     K_cache = K_cache[cache_indices, :]
     V_cache = V_cache[cache_indices, :]
-    K_cache = K_cache.reshape(batch_size, len(cache_indices), num_heads, dim // num_heads).permute(0, 2, 1, 3)
-    V_cache = V_cache.reshape(batch_size, len(cache_indices), num_heads, dim // num_heads).permute(0, 2, 1, 3)
+    K_cache = K_cache.reshape(batch_size, 2, num_heads, dim // num_heads).permute(0, 2, 1, 3)
+    V_cache = V_cache.reshape(batch_size, 2, num_heads, dim // num_heads).permute(0, 2, 1, 3)
     new_K = torch.concat([K_cache, K.unsqueeze(2)], dim=2)
     new_V = torch.concat([V_cache, V.unsqueeze(2)], dim=2)
     S = torch.matmul(
@@ -44,10 +50,12 @@ S1, P1, O1 = reference_kv_MHA(Q, K, V, K_cache, V_cache, cache_indices)
 end = time.perf_counter()
 print(f"reference MHA implementation: {end - beg:0.4f} secs")
 beg = time.perf_counter()
-S2, P2, O2 = paged_kv_attention_forward(Q, K, V, K_cache, V_cache, cache_indices, num_heads)
+S2, P2, O2 = paged_kv_attention_forward(Q, K, V, K_cache, V_cache, cache_indices, offsets, num_heads)
 end = time.perf_counter()
 print(f"CUDA MHA implementation: {end - beg:0.4f} secs")
 
+print(S1)
+print(S2)
 print("======(Max diff) Accuracy compared to ref implementation======")
 print(torch.abs(S1 - S2).max())
 print(torch.abs(P1 - P2).max())
